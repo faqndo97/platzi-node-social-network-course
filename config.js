83dc833528a7f0e6edc6b16c615b33ee8cee9778
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-03-21 22:10:57 -0300

    Add mysql integration

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'secret'
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || 'remotemysql.com',
+    user: process.env.MYSQL_USER || '73ZPHee3tO',
+    password: process.env.MYSQL_PASSWORD || 'ph7BveGtIj',
+    database: process.env.MYSQL_DATABASE || '73ZPHee3tO',
   }
 }
