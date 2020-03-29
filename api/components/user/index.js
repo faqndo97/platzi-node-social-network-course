ecd6d28740e653943cdfc6833ef0da7d6ea31eed
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-03-29 19:01:36 -0300

    add dynamic way to use remote db or local db

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,4 +1,11 @@
-const store = require('../../../store/remote-mysql');
+let store;
+const config = require('../../../config');
 const controller = require('./controller');
+
+if (config.remoteDB) {
+  store = require('../../../store/remote-mysql');
+} else {
+  store = require('../../../store/mysql');
+}
 
 module.exports = controller(store);
